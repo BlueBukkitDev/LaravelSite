897ef3fac91e792196b33835a0033a69692954d3
Jonathan Bruce <bluebukkitdev@gmail.com>
2023-12-12 17:13:22 -0500

    first commit

---
+++ public/sidebar.php
@@ -0,0 +1,7 @@
+<div class="sidebar">
+    <div class="navbar-item">Section 1</div>
+    <div class="navbar-item">Section 2</div>
+    <div class="navbar-item">Section 3</div>
+    <div class="navbar-item">Section 4</div>
+    <div class="navbar-item">Section 5</div>
+</div>

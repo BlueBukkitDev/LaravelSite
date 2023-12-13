1d948d72741b7516f3a92a90e529cb771ae7d07c
Jonathan Bruce <bluebukkitdev@gmail.com>
2023-12-13 17:50:55 -0500

    Fixed closing tag warning on by <br/> elements.
    Also just a test to see how all of git works in PHPStorm.

--- resources/views/science.blade.php
+++ resources/views/science.blade.php
@@ -10,9 +10,9 @@
         <div class="mid">
             <div class="content">
                 <p>All kinds of content could go inside this div. This is a basic paragraph made up of all kinds of generic lorem ipsum content, like what you just read and continue to read. This paragraph needs to be long enough to properly display what is going on in this div.
-                    </br>
-                    </br>
-                    </br>
+                    <br/>
+                    <br/>
+                    <br/>
                     All the way down here, a new paragraph, within the same tags. </p>
             </div>
         </div>
